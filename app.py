5ed780227390a94166c8b6c061ed2d678e796c5d
Flavio Vicentin <flavicentin10@gmail.com>
2026-04-26 15:52:20 -0300

    logout flask

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user, current_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -35,7 +35,16 @@ def login():
         
     return jsonify({"message": "credenciais invalidas"}), 400
 
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso"})
+
+
+
 @app.route("/helloworld", methods=["GET"])
+@login_required
 def helloworld():
     return "hello world"
 
